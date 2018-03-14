4aa749f55f2ec294ee4e54d94d33d854218b7198
Wojtek <Wojtek.zarnowka@gmail.com>
2018-03-14 19:19:50 +0100

    As2 Language

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("721011081081113287111114108100");
     }
 }
