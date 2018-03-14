7da105e8691e9751e0749ec6bf2440ee63319da7
Kabau97 <dominik.cekus@gmail.com>
2018-03-14 19:20:35 +0100

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Moni Dziko Lapansi!");
+        System.out.println("Moni Dziko Lapansi !");
     }
 }
